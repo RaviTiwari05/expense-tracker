01e1302496f8ceda67899f362d1574b7357ba55d
RaviTiwari05 <156442621+RaviTiwari05@users.noreply.github.com>
2026-02-16 17:33:52 +0530

    Update API baseURL to include '/expenses'

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { v4 as uuidv4 } from "uuid";
 
 const API = axios.create({
-  baseURL: "https://expense-tracker-6ame.onrender.com"
+  baseURL: "https://expense-tracker-6ame.onrender.com/expenses"
 });
 
 export const createExpense = async (data) => {
